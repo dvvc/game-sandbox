22e184aa2e4a4c0500f19280976b475001b2de8e
David Villegas <dville00@gmail.com>
2021-02-17 00:10:46 -0500

    Initial HTML-based HUD implementation

--- dist/game-sandbox.js
+++ dist/game-sandbox.js
@@ -6,7 +6,8 @@ var KEY_MAP = {
   ArrowLeft: "left",
   KeyX: "b1",
   KeyZ: "b2",
-  Backquote: "hud"
+  Backquote: "hud",
+  Enter: "enter"
 };
 function initInput() {
   let currentInput = {};
@@ -30,7 +31,8 @@ function initInput() {
     currentInputP
   };
 }
-function readInput(internalInput, env) {
+function readInput(env) {
+  let internalInput = env.engine.input;
   env.input = Object.assign({}, internalInput.currentInput, internalInput.currentInputP);
 }
 function clearFrameInput(internalInput) {
@@ -39,16 +41,84 @@ function clearFrameInput(internalInput) {
   });
 }
 
+// lib/hud.js
+var HUD_COMMANDS = ["Toggle FPS", "Take state snapshot", "Load state snapshot"];
+var COMMAND_BG = "#555555";
+var COMMAND_FG = "#eeeeee";
+function buildHudElement(boundingBox) {
+  let hudElement = document.createElement("div");
+  hudElement.style.position = "absolute";
+  hudElement.style.top = boundingBox.top + "px";
+  hudElement.style.left = boundingBox.left + "px";
+  hudElement.style.width = boundingBox.width + "px";
+  hudElement.style.height = boundingBox.height + "px";
+  hudElement.style.backgroundColor = COMMAND_BG;
+  hudElement.style.opacity = 0;
+  hudElement.style.transition = "opacity 0.3s ease";
+  let hudCommandList = document.createElement("ul");
+  hudCommandList.style.color = COMMAND_FG;
+  hudCommandList.style.listStyle = "none";
+  hudCommandList.style.padding = "0 20px";
+  hudCommandList.style.marginTop = "40px";
+  hudCommandList.style.fontFamily = "monospace";
+  HUD_COMMANDS.forEach((cmd) => {
+    let commandItem = document.createElement("li");
+    commandItem.innerHTML = cmd;
+    commandItem.style.lineHeight = 1.6;
+    hudCommandList.appendChild(commandItem);
+  });
+  hudCommandList.children.item(0).style.backgroundColor = COMMAND_FG;
+  hudCommandList.children.item(0).style.color = COMMAND_BG;
+  hudElement.appendChild(hudCommandList);
+  return hudElement;
+}
+function initHud(boundingBox) {
+  let hudElement = buildHudElement(boundingBox);
+  document.body.appendChild(hudElement);
+  return {
+    active: false,
+    selectedCommand: 0,
+    element: hudElement
+  };
+}
+function toggleHud(hud) {
+  hud.active = !hud.active;
+  hud.element.style.opacity = Number(hud.active);
+}
+function updateHudCommandSelection(hud, input) {
+  let newCommandIndex = hud.selectedCommand;
+  if (input.downp) {
+    newCommandIndex = (hud.selectedCommand + 1) % HUD_COMMANDS.length;
+  } else if (input.upp) {
+    newCommandIndex = (hud.selectedCommand - 1 + HUD_COMMANDS.length) % HUD_COMMANDS.length;
+  }
+  if (newCommandIndex !== hud.selectedCommand) {
+    let commandList = hud.element.firstChild;
+    commandList.children.item(hud.selectedCommand).style.backgroundColor = null;
+    commandList.children.item(hud.selectedCommand).style.color = COMMAND_FG;
+    commandList.children.item(newCommandIndex).style.backgroundColor = COMMAND_FG;
+    commandList.children.item(newCommandIndex).style.color = COMMAND_BG;
+    hud.selectedCommand = newCommandIndex;
+  }
+}
+function updateHud(hud, input) {
+  if (input.enterp) {
+    return hud.selectedCommand;
+  }
+  updateHudCommandSelection(hud, input);
+}
+
 // lib/engine.js
 var OPT_DEFAULTS = {
   width: 600,
   height: 600,
   canvasId: "canvas"
 };
-function initEngine() {
+function initEngine(canvasBoundingBox) {
   return {
     initialized: false,
-    input: initInput()
+    input: initInput(),
+    hud: initHud(canvasBoundingBox)
   };
 }
 function initEnv(opts) {
@@ -66,22 +136,34 @@ function initEnv(opts) {
     width,
     height,
     input: {},
-    delta: 0
+    delta: 0,
+    engine: initEngine(canvasEl.getBoundingClientRect())
   };
 }
-function initGame(state, engine, env) {
+function initGame(state, env) {
   let updatedState = state;
   let lastTime = 0;
   (function gameLoop(time) {
     env.delta = (time - lastTime) / 1e3;
-    readInput(engine.input, env);
-    updatedState = engine.draw(updatedState, env);
-    clearFrameInput(engine.input);
+    readInput(env);
+    if (env.input.hudp) {
+      toggleHud(env.engine.hud);
+    }
+    if (env.engine.hud.active) {
+      let selectedCommand = updateHud(env.engine.hud, env.input);
+      if (selectedCommand !== void 0) {
+        toggleHud(env.engine.hud);
+        console.log("COMMMAND!", selectedCommand);
+      }
+    } else {
+      updatedState = env.engine.draw(updatedState, env);
+    }
+    clearFrameInput(env.engine.input);
     lastTime = time;
     window.requestAnimationFrame(gameLoop);
   })(0);
 }
-function startGameClient(moduleUrl, engine, env) {
+function startGameClient(moduleUrl, env) {
   let protocol = location.protocol === "https" ? "wss" : "ws";
   let ws = new WebSocket(`${protocol}://${location.host}?module=${moduleUrl}`);
   ws.addEventListener("message", async (event) => {
@@ -96,12 +178,13 @@ function startGameClient(moduleUrl, engine, env) {
       if (!module.setup || !module.draw) {
         throw new Error(`Invalid module at ${file}`);
       }
+      let engine = env.engine;
       engine.setup = module.setup;
       engine.draw = module.draw;
       if (!engine.initialized) {
         let state = engine.setup(env);
         engine.initialized = true;
-        initGame(state, engine, env);
+        initGame(state, env);
       }
     }
   });
@@ -112,9 +195,8 @@ async function runGame(opts) {
     throw new Error(`Must provide a moduleUrl`);
   }
   window.addEventListener("load", () => {
-    let engine = initEngine();
     let env = initEnv(opts);
-    startGameClient(opts.moduleUrl, engine, env);
+    startGameClient(opts.moduleUrl, env);
   });
 }
 export {
